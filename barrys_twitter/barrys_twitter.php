b26e6f064c53ec0a1e75a9a5db456aa0707fd592
Barry Carlyon <barry@barrycarlyon.co.uk>
2013-09-07 19:42:33 +0100

    // use magic wordpress object pie cakeness

--- barrys_twitter/barrys_twitter.php
+++ barrys_twitter/barrys_twitter.php
@@ -10,11 +10,15 @@ Version: 0.0.1
 */
 
 class barrys_twitter {
+    protected $_settings;
+
     function __construct() {
-        $barrys_twitter = get_option('barrys_twitter', false);
+        // use magic wordpress object pie cakeness
+        $barrys_twitter = maybe_unserialize(get_option('barrys_twitter', false));
         if (!$barrys_twitter) {
             $barrys_twitter = new barrys_twitter_settings();
         }
+        $this->_settings = $barrys_twitter;
 
         if (is_admin()) {
             $this->_admin();
@@ -30,7 +34,58 @@ class barrys_twitter {
     }
 
     function admin_page() {
-        echo 'hey hey hey';
+        echo '<pre>'.print_r($this->_settings,true).'</pre>';
+        if ($_POST) {
+            foreach ($this->_settings as $var => $val) {
+                $this->_settings->$var = isset($_POST[$var]) ? $_POST[$var] : $val;
+            }
+            $this->_settings->saveOptions();
+
+            echo '
+            <div id="setting-error-settings_updated" class="updated settings-error">
+                <p><strong>Settings saved.</strong></p>
+            </div>
+            ';
+        }
+
+        echo '
+<div class="wrap">
+<form action="" method="post">
+    <fieldset>
+        <div id="icon-options-general" class="icon32"><br></div>
+        <h2>Barrys Twitter Settings</h2>
+        <table class="form-table">
+';
+        // start state
+
+        $state = $this->_settings->getState();
+
+        switch ($state) {
+            case 1:
+                echo '
+            <tr valign="top">
+                <th scope="row"><label for="consumer_key">Consmer Key</label></th>
+                <td><input type="text" name="consumer_key" id="consumer_key" /></td>
+            </tr>
+            <tr valign="top">
+                <th scope="row"><label for="consumer_secret">Consmer Secret</label></th>
+                <td><input type="text" name="consumer_secret" id="consumer_secret" /></td>
+            </tr>
+';
+                break;
+        }
+
+        // end state
+        echo '
+        </table>
+        <p class="submit">
+            <input type="submit" value="Update" class="button button-primary" />
+        </p>
+    </fieldset>
+</form>
+</div>
+';
+
     }
 
 }
@@ -45,8 +100,26 @@ class barrys_twitter_settings {
     var $oauth_token_secret;
 //    var $oauth_verifier;
 
-    function save_options() {
-        $data = json_encode($this);
-        update_option('barrys_twitter');
+    function saveOptions() {
+        $data = serialize($this);
+        update_option('barrys_twitter', $data);
+    }
+
+    function getState() {
+        if ($this->consumer_key && $this->consumer_secret) {
+            // have basic keys
+            if ($this->oauth_token && $this->oauth_token_secret) {
+                // have oauth keys
+
+                // verify then
+                return 3;
+            }
+
+            // need to loop the loop
+            return 2;
+        }
+
+        // need basoics
+        return 1;
     }
 }
